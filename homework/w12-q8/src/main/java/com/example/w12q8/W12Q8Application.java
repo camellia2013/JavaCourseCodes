d449718c2a8ce6c69babcb4b873fa0e63b07dcd5
lnma <lina.ma@99wuxian.com>
2021-06-18 11:04:40 +0800

    w12-q8

--- homework/w12-q8/src/main/java/com/example/w12q8/W12Q8Application.java
+++ homework/w12-q8/src/main/java/com/example/w12q8/W12Q8Application.java
@@ -7,6 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 
+/**
+ * 7.（选做）基于数据库的订单表，模拟消息队列处理订单：
+ *
+ * 	* 一个程序往表里写新订单，标记状态为未处理 (status=0);
+ * 	*
+ * 另一个程序每隔 100ms 定时从表里读取所有 status=0 的订单，打印一下订单数据，然后改成完成 status=1；
+ * 	*
+ * （挑战☆）考虑失败重试策略，考虑多个消费程序如何协作。
+ *
+ * 8.（选做）将上述订单处理场景，改成使用 ActiveMQ 发送消息处理模式。
+ */
 @SpringBootApplication
 public class W12Q8Application {
 
